986f6283dae8b32113939bcb687a1c8713a97cb9
Himanshu <himanshucser@gmail.com>
2026-08-03 18:46:47 +0530

    bug fix

--- app/Repositories/SettingRepository.php
+++ app/Repositories/SettingRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Setting;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class SettingRepository
